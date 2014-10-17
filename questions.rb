4c7496ea23e58342da4e5c802bb3b2f32cb2882e
Thomas Beeley <tbeeley@gmail.com>
2014-10-17 14:42:21 +0100

    tricky ones lef

--- questions.rb
+++ questions.rb
@@ -191,30 +191,41 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  exclusions = %w(a the and of)
+  words = string.split
+  words.first.capitalize!
+  words.map do |word|
+    exclusions.include?(word) ? word : word.capitalize
+  end.join(' ')
 end
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+  range.max
 end
 
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.exclude_end
 end
 
 # get the square root of a number
 def square_root_of(number)
+  Math.sqrt(number)
 end
 
 # count the number of words in a file
 def word_count_a_file(file_path)
+  File.read(file_path).split.count
 end
 
 # --- tougher ones ---
